80dfe28357d726ac6334822feed2d881f30ff6f0
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:08 +0200

    feat(cli): start config with CONFIG_PATH

---
+++ src/cli/commands/config.ts
@@ -0,0 +1,9 @@
+import type { Command } from 'commander'
+import chalk from 'chalk'
+import { readConfig, writeConfig } from '../../core/config.js'
+import { printJson, printError, printSuccess } from '../ui/output.js'
+import type { SkillForgeConfig } from '../../core/config.js'
+import os from 'node:os'
+import path from 'node:path'
+
+const CONFIG_PATH = () => path.join(os.homedir(), '.skillforge', 'config.json')
