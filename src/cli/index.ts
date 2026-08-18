db5e5840c744d7a56d8fe8e41120ef7c9ec787d9
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:09 +0200

    feat(cli): start index with require

---
+++ src/cli/index.ts
@@ -0,0 +1,14 @@
+import { Command } from 'commander'
+import { createRequire } from 'node:module'
+import { registerInstall } from './commands/install.js'
+import { registerUninstall } from './commands/uninstall.js'
+import { registerList } from './commands/list.js'
+import { registerValidate } from './commands/validate.js'
+import { registerSuggest } from './commands/suggest.js'
+import { registerPublish } from './commands/publish.js'
+import { registerSearch } from './commands/search.js'
+import { registerSkillset } from './commands/skillset.js'
+import { registerUpdate } from './commands/update.js'
+import { registerConfig } from './commands/config.js'
+
+const require = createRequire(import.meta.url)
