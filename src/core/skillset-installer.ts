48d039b1b63f1b2e9a57b8cdf7c13caf7216442f
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:04 +0200

    feat(core): start skillset-installer with SkillsetInstallOptions

---
+++ src/core/skillset-installer.ts
@@ -0,0 +1,17 @@
+import { cp, rm, mkdir, stat } from 'node:fs/promises'
+import path from 'node:path'
+import type { ScopeLevel } from '../types/scope.js'
+import type { InstalledSkillset } from '../types/manifest.js'
+import type { SkillsetValidationResult, RemoteSkillRef } from '../types/skillset.js'
+import type { InstallResult } from './installer.js'
+import { validateSkillset, SKILLSET_SPEC_VERSION } from './skillset-validator.js'
+import { resolveScope } from './resolver.js'
+import { addSkillsetToManifest, removeSkillsetFromManifest, readManifest } from './manifest.js'
+import { installFromPath, uninstallSkill } from './installer.js'
+
+export interface SkillsetInstallOptions {
+  scope: ScopeLevel
+  force?: boolean
+  dryRun?: boolean
+  sourceUrl?: string
+}
