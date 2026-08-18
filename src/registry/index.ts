c6224b5b22fb0f329ca9089154567ef3bd790bd7
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:06 +0200

    feat(registry): extend index

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,1 +1,9 @@
 export { installFromGitUrl, parseGitUrl } from './sources/github.js'
+export {
+  searchRegistry,
+  getSkillInstallInfo,
+  getSkill,
+  publishSkill,
+  updateSkill,
+  deleteSkill,
+} from './sources/registry.js'
