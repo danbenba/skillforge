9da52416a9962071b04d36beb9b6f18f6533820d
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:12 +0200

    test(skillset-validator): add fixtures helper

--- tests/unit/skillset-validator.test.ts
+++ tests/unit/skillset-validator.test.ts
@@ -4,3 +4,4 @@ import { fileURLToPath } from 'node:url'
 import { validateSkillset } from '../../src/core/skillset-validator.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const fixtures = (name: string) => path.join(__dirname, '..', 'fixtures', name)
