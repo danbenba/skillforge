3d166561909ffd662c6bd1819c886460693d9fee
danbenba <danbenba4660@gmail.com>
2026-08-18 09:59:13 +0200

    feat(core): define ServerEnv

--- src/core/env.ts
+++ src/core/env.ts
@@ -3,3 +3,14 @@ import { config as loadDotenv } from 'dotenv'
 loadDotenv({ quiet: true })
 
 export type RuntimeMode = 'dev' | 'prod'
+
+export interface ServerEnv {
+  mode: RuntimeMode
+  host: string
+  port: number
+  publicUrl: string
+  registryUrl?: string
+  authToken?: string
+  bundleFileLimit: number
+  bundleTotalLimit: number
+}
